b4addfdd56f78b54c4ef207d2d836f4c1e6fd5df
rcossette2 <r_cossette@apple.com>
2023-10-22 09:36:18 -0600

    updates

---
+++ .next/server/app/_not-found_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found"]={"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/Volumes/Projects/Personal/Websites/personal-site/src/app/not-found":[]}}
